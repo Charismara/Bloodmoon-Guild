cb614576fa52fc3a1945b98570f9acfdd82abe32
Charismara <buck.tim6@googlemail.com>
2016-11-15 19:29:28 +0100

    update

--- src/main/java/de/blutmondgilde/bloodguild/BloodGuild.java
+++ src/main/java/de/blutmondgilde/bloodguild/BloodGuild.java
@@ -2,6 +2,7 @@ package de.blutmondgilde.bloodguild;
 
 import de.blutmondgilde.bloodguild.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -16,22 +17,23 @@ public class BloodGuild {
 	public static final String VERSION = "0.0.1a";
 	public static final String NAME = "Blood Guild";
 
-	//Instance register
+	// Instance register
 	@Instance(MODID)
 	public static BloodGuild instance = new BloodGuild();
-	
-	//Proxy register
+
+	// Proxy register
 	@SidedProxy(modId = MODID, serverSide = "de.blutmondgilde.bloodguild.proxy.CommonProxy", clientSide = "de.blutmondgilde.bloodguild.proxy.ClientProxy")
 	public static CommonProxy proxy = new CommonProxy();
-	
+
+	// Forge Start-up events (Chronologisch sortiert)
 	@EventHandler
 	public void preinit(FMLPreInitializationEvent event) {
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 	}
-	
+
 	@EventHandler
 	public void postinit(FMLPostInitializationEvent event) {
 		proxy.registerModels();
