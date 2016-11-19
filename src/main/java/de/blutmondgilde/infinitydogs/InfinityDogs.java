96cda37702f1b3412fa1bded7561b04ef5c3de1e
Charismara <buck.tim6@googlemail.com>
2016-11-19 16:54:51 +0100

    update

--- src/main/java/de/blutmondgilde/infinitydogs/InfinityDogs.java
+++ src/main/java/de/blutmondgilde/infinitydogs/InfinityDogs.java
@@ -1,6 +1,6 @@
-package de.blutmondgilde.bloodguild;
+package de.blutmondgilde.infinitydogs;
 
-import de.blutmondgilde.bloodguild.proxy.CommonProxy;
+import de.blutmondgilde.infinitydogs.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -11,18 +11,18 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = BloodGuild.MODID, version = BloodGuild.VERSION, name = BloodGuild.NAME)
-public class BloodGuild {
-	public static final String MODID = "bloodguild";
+@Mod(modid = InfinityDogs.MODID, version = InfinityDogs.VERSION, name = InfinityDogs.NAME)
+public class InfinityDogs {
+	public static final String MODID = "infinitydogs";
 	public static final String VERSION = "0.0.1a";
-	public static final String NAME = "Blood Guild";
+	public static final String NAME = "Infinity Dogs";
 
 	// Instance register
 	@Instance(MODID)
-	public static BloodGuild instance = new BloodGuild();
+	public static InfinityDogs instance = new InfinityDogs();
 
 	// Proxy register
-	@SidedProxy(modId = MODID, serverSide = "de.blutmondgilde.bloodguild.proxy.CommonProxy", clientSide = "de.blutmondgilde.bloodguild.proxy.ClientProxy")
+	@SidedProxy(modId = MODID, serverSide = "de.blutmondgilde.infinitydogs.proxy.CommonProxy", clientSide = "de.blutmondgilde.infinitydogs.proxy.ClientProxy")
 	public static CommonProxy proxy = new CommonProxy();
 
 	
